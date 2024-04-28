3885d8c6f360b77aae71d98399b509178e21ef74
Merleoleona <150333964+Merleoleona@users.noreply.github.com>
2024-04-28 15:16:09 -0700

    initial commit

---
+++ PattyMaker/Assets/DragDrop.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler
+{
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown");
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag");
+        rectTransform.anchoredPosition += eventData.delta;
+    }
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            Debug.Log("OnPointerDown");
+        canvasGroup.alpha = .6f;
+        canvasGroup.blocksRaycasts = false;
+        }
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            Debug.Log("OnEndDrag");
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+        }
+        public void OnPoionterDown(PointerEventData eventData)
+        {
+            Debug.Log("OnPointerDrag");
+        }
+        public void OnDrop(PointerEventData eventData)
+        {
+        throw new System.NotImplementedException();
+        }
+
+
+}
+
+
